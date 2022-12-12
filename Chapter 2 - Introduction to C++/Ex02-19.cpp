1f3462ce56e3a4e2734dd96abcdc1fe906228418
Tri <trimpham98@gmail.com>
2022-12-12 12:43:01 -0800

    Add Example 2-19

---
+++ Chapter 2 - Introduction to C++/Ex02-19.cpp
@@ -0,0 +1,12 @@
+// This program shows variable initialization.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int month = 2, days = 28;
+
+    cout << "Month " << month << " has " << days << " days." << endl;
+
+    return 0;
+}
