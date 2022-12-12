69e16548686c6210a419954129b4256a006c92db
Tri <trimpham98@gmail.com>
2022-12-12 08:33:04 -0800

    Add Example 2-13

---
+++ Chapter 2 - Introduction to C++/Ex02-13.cpp
@@ -0,0 +1,13 @@
+// This program demonstrates the close relationship between characters and integers.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter = 65;
+    cout << letter << endl;
+    letter = 66;
+    cout << letter << endl;
+
+    return 0;
+}
