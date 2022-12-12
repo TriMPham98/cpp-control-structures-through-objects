020555d152a9528fe9b62866162e4a452e7f33ee
Tri <trimpham98@gmail.com>
2022-12-12 08:30:12 -0800

    Add Example 2-12

---
+++ Chapter 2 - Introduction to C++/Ex02-12.cpp
@@ -0,0 +1,14 @@
+// This program works with characters.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter = 'A';
+
+    cout << letter << endl;
+    letter = 'B';
+    cout << letter << endl;
+
+    return 0;
+}
