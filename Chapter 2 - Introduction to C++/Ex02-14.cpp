f52304c4261532e83e102dcd438280a510cc3846
Tri <trimpham98@gmail.com>
2022-12-12 08:40:06 -0800

    Add Example 2-14

---
+++ Chapter 2 - Introduction to C++/Ex02-14.cpp
@@ -0,0 +1,14 @@
+// This program uses character literals.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter = 'A';
+
+    cout << letter << '\n';
+    letter = 'B';
+    cout << letter << '\n';
+
+    return 0;
+}
