ff376e20e934a999f72be43e64a8cea3a427e63a
Tri <trimpham98@gmail.com>
2022-12-12 22:10:37 -0800

    Add Example 2-27

---
+++ Chapter 2 - Introduction to C++/Ex02-27.cpp
@@ -0,0 +1,16 @@
+/*
+PROGRAM: PAYROLL.CPP
+Written by Tri Pham
+This program calculates company payroll
+Last modification: 8/20/2017
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double payRate;     // Holds the hourly pay rate
+    double hours;       // Holds the hours worked
+    int employeeNumber; // Holds the employee number
+}
