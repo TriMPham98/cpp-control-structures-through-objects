1b04a3c0115e38bedde17a986b0e5ab0ee9f3501
Tri <trimpham98@gmail.com>
2022-12-12 21:55:36 -0800

    Add Example 2-25

---
+++ Chapter 2 - Introduction to C++/Ex02-25.cpp
@@ -0,0 +1,25 @@
+// This program converts seconds to minutes and seconds.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // The total seconds is 125.
+    int totalSeconds = 125;
+
+    // Variables for the minutes and seconds.
+    int minutes, seconds;
+
+    // Get the number of minutes.
+    minutes = totalSeconds / 60;
+
+    // Get the remaining seconds.
+    seconds = totalSeconds % 60;
+
+    // Display the results.
+    cout << totalSeconds << " seconds is equivalent to: " << endl;
+    cout << "Minutes: " << minutes << endl;
+    cout << "Seconds: " << seconds << endl;
+
+    return 0;
+}
