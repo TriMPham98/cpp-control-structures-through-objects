4844d594942f2fa9dadba3733368227f9a10e67b
Tri <trimpham98@gmail.com>
2022-12-13 08:16:57 -0800

    Add salesTax body

--- Chapter 2 - Introduction to C++/salesTax.cpp
+++ Chapter 2 - Introduction to C++/salesTax.cpp
@@ -1,3 +1,19 @@
 // Write a program that will compute the total sales tax on a $95 purchase.
 // Assume the state sales tax is 4%, and the county sales tax is 2%.
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double purchasePrice = 95.00;
+    double stateTax = 0.04;
+    double countyTax = 0.02;
+
+    double totalTax = (purchasePrice * stateTax) + (purchasePrice * countyTax);
+
+    cout << "The total tax of a $95 purchase is $" << fixed << setprecision(2) << totalTax << endl;
+
+    return 0;
+}
