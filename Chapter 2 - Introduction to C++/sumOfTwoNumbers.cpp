2377ef682a542e88a07b65407aabad143df0e5b9
Tri <trimpham98@gmail.com>
2022-12-12 22:53:10 -0800

    Add sumOfTwoNumbers

---
+++ Chapter 2 - Introduction to C++/sumOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+// Write a program that stores the integers 50 and 100 in variables,
+// and stores the sum of these two in a variable named total.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 50, num2 = 100;
+    int sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
