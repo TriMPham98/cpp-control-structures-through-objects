9882e4a23fa2783613276be512efd6126b1cf882
Tri <trimpham98@gmail.com>
2022-12-12 22:08:15 -0800

    Add Example 2-26

---
+++ Chapter 2 - Introduction to C++/Ex02-26.cpp
@@ -0,0 +1,13 @@
+// PROGRAM: PAYROLL.CPP
+// Written by Herbert Dorfman
+// This program calculates company payroll
+// Last modification: 8/20/2017
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double payRate;     // Holds the hourly pay rate
+    double hours;       // Holds the hours worked
+    int employeeNumber; // Holds the employee number
+}
