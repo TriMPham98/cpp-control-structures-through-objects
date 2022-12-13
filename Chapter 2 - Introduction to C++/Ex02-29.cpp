7254f6eb4b3c93d1576c885c743d4b6ae1d4b19e
Tri <trimpham98@gmail.com>
2022-12-12 22:15:37 -0800

    Add Example 2-29

---
+++ Chapter 2 - Introduction to C++/Ex02-29.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;int main(){double shares = 220.0;
+double avgPrice=14.68;cout<<"There were "<<shares<<" shares sold at $"<<avgPrice<<" per share."<<endl;return 0;}
