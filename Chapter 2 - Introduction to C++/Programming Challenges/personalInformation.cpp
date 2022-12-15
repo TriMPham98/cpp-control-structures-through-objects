3df7262fb6bdb655a7fc5b976f1863bad637296d
Tri <trimpham98@gmail.com>
2022-12-14 17:03:01 -0800

    Add personal information program body

--- Chapter 2 - Introduction to C++/Programming Challenges/personalInformation.cpp
+++ Chapter 2 - Introduction to C++/Programming Challenges/personalInformation.cpp
@@ -5,3 +5,15 @@
 // Your college major
 // Use only a single cout statement to display all of this information.
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Name: Tri Pham\n"
+         << "Address: 123 Main Street, San Jose, CA, 12345\n"
+         << "Telephone number: 123-456-789\n"
+         << "College major: Computer Science\n";
+
+    return 0;
+}
