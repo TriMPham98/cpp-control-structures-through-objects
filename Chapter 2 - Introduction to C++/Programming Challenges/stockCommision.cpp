b62a5d5fb70d4d2bb3309c137e812cd8a89b9562
Tri <trimpham98@gmail.com>
2022-12-15 10:52:47 -0800

    Add stockCommission.cpp

---
+++ Chapter 2 - Introduction to C++/Programming Challenges/stockCommision.cpp
@@ -0,0 +1,7 @@
+// Kathryn bought 750 shares of stock at a price of $35.00 per share. She must pay her
+// stockbroker a 2 percent commission for the transaction. Write a program that calculates
+// and displays the following:
+// - The amount paid for the stock alone (without the commission.)
+// - The amount of the commission.
+// - The total amount paid (for the stock plus the commission).
+
