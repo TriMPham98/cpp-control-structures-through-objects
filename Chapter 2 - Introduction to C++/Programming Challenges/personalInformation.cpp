e5c251a01d1040e1a17700a3b59b0762eb01d6d7
Tri <trimpham98@gmail.com>
2022-12-14 17:00:58 -0800

    Initial commit

---
+++ Chapter 2 - Introduction to C++/Programming Challenges/personalInformation.cpp
@@ -0,0 +1,7 @@
+// Write a program that displays the following pieces of information, each on a separate line:
+// Your name
+// Your address, with city, state, and ZIP code
+// Your telephone number
+// Your college major
+// Use only a single cout statement to display all of this information.
+
