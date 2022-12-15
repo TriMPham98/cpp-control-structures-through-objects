c893ae9dbd6dc2d703969387418c099da11a9f06
Tri <trimpham98@gmail.com>
2022-12-14 17:06:35 -0800

    Add trianglePattern.cpp

---
+++ Chapter 2 - Introduction to C++/Programming Challenges/trianglePattern.cpp
@@ -0,0 +1,18 @@
+// Write a program that displays the following pattern on the screen:
+//    *
+//   ***
+//  *****
+// *******
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "    *    " << endl;
+    cout << "   ***   " << endl;
+    cout << "  *****  " << endl;
+    cout << " ******* " << endl;
+
+    return 0;
+}
