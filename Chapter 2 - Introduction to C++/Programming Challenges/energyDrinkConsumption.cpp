df01494e86cbaee1a82a00423097ab1c7caba34e
Tri <trimpham98@gmail.com>
2022-12-15 11:04:53 -0800

    Add program description

---
+++ Chapter 2 - Introduction to C++/Programming Challenges/energyDrinkConsumption.cpp
@@ -0,0 +1,8 @@
+// A soft drink company recently surveyed 16,500 of its customers and found that 
+// approximately 15 percent of those surveyed purchase one or more energy drinks per week. 
+// Of those customers who purchase energy drinks, approximately 58 percent of them prefer
+// citrus-flavored energy drinks. Write a program that displays rhe following:
+// - The approximate number of customers in the survey who purchase one or more
+// energy drinks per week.
+// - The approximate number of customers in the survey who prefer citrus-flavored
+// energy drinks.
