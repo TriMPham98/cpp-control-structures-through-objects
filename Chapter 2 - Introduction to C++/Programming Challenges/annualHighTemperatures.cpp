c6e3f4e16cffef63d4dddf0ccb0741e37f5dde4d
Tri <trimpham98@gmail.com>
2022-12-15 20:52:56 -0800

    Add program description

---
+++ Chapter 2 - Introduction to C++/Programming Challenges/annualHighTemperatures.cpp
@@ -0,0 +1,5 @@
+// The average July high temperarure is 85 degrees Fahrenheit in New York City, 88 degrees
+// Fahrenheit in Denver, and 106 degrees Fahrenheit in Phoenix. Write a program that
+// calculates and reports what the new average July high temperature would be for each of
+// these cities if temperatures rise by 2 percent
+
