34743b961de5b23816d552ed207d9d04400152bf
Tri <trimpham98@gmail.com>
2022-12-15 22:45:39 -0800

    Add howMuchPaint.cpp

--- Chapter 2 - Introduction to C++/Programming Challenges/howMuchPaint.cpp
+++ Chapter 2 - Introduction to C++/Programming Challenges/howMuchPaint.cpp
@@ -1,5 +1,16 @@
 // A particular brand of paint covers 340 square feet per gallon.
-// Write a program to determine and report approximately how many 
-// gallons of paint will be needed to paint two coats on a wooden fence 
+// Write a program to determine and report approximately how many
+// gallons of paint will be needed to paint two coats on a wooden fence
 // that is 6 feet high and 100 feet long.
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double sqFtPerGalPaint = 340;
+    double areaToPaint = 6 * 100;
+
+    cout << "You will need " << areaToPaint / sqFtPerGalPaint
+         << " gallons of paint to cover a fence that is 6ft high and 100 ft long." << endl;
+}
