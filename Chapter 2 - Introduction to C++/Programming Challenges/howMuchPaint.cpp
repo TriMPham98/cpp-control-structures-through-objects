48089c6142ea7c24bd688a4929e3d254d5122963
Tri <trimpham98@gmail.com>
2022-12-15 21:51:00 -0800

    Add program description

---
+++ Chapter 2 - Introduction to C++/Programming Challenges/howMuchPaint.cpp
@@ -0,0 +1,5 @@
+// A particular brand of paint covers 340 square feet per gallon.
+// Write a program to determine and report approximately how many 
+// gallons of paint will be needed to paint two coats on a wooden fence 
+// that is 6 feet high and 100 feet long.
+
