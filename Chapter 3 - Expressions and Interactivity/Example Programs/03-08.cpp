381d0b43a46837c3c0d37f0cc67530d0065df423
Tri <trimpham98@gmail.com>
2022-12-16 23:30:16 -0800

    Add exercise 03-08

--- Chapter 3 - Expressions and Interactivity/Example Programs/03-08.cpp
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-08.cpp
@@ -8,9 +8,9 @@ int main()
 {
     float test;
 
-    test = 2.0e38 * 1000; // Should overflow test
+    test = 2.0e38 * 1000; // Overflow test
     cout << "Overflow output: " << test << endl;
-    test = 2.0e-38 / 2.0e38; // Should underflow test
+    test = 2.0e-38 / 2.0e38; // Underflow test
     cout << "Underflow output: " << test << endl;
 
     return 0;
