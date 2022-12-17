1fe09f6e29a9cf32a097c020d90ec950e1c1d24d
Tri <trimpham98@gmail.com>
2022-12-16 23:29:31 -0800

    Add exercise 03-07

---
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-08.cpp
@@ -0,0 +1,17 @@
+// This program can be used to see how you system handles
+// floating point overflow and underflow.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float test;
+
+    test = 2.0e38 * 1000; // Should overflow test
+    cout << "Overflow output: " << test << endl;
+    test = 2.0e-38 / 2.0e38; // Should underflow test
+    cout << "Underflow output: " << test << endl;
+
+    return 0;
+}
