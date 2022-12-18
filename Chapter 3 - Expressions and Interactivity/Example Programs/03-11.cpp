2ea28f79b4656d194642de3fe2303d94417d26a0
Tri <trimpham98@gmail.com>
2022-12-18 14:01:33 -0800

    Initialize variables

---
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-11.cpp
@@ -0,0 +1,19 @@
+// This program tracks the inventory of three widget stores that
+// opened at the same time. Each store started with the same number
+// of widgets in inventory. By subtracting the number of widgets
+// each store has sold from its inventory, the current inventory
+// can be calculated.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int begInv, // Beginning inventory for all stores
+        sold,   // Number of widgets sold
+        store1, // Store 1's inventory
+        store2, // Store 2's inventory
+        store3; // Store 3's inventory
+
+    
+}
