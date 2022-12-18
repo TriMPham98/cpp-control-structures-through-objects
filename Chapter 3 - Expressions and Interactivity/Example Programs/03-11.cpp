e6c88e65bf2bbd582542099ca686a95886985af4
Tri <trimpham98@gmail.com>
2022-12-18 14:08:42 -0800

    Add user input and print statements

--- Chapter 3 - Expressions and Interactivity/Example Programs/03-11.cpp
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-11.cpp
@@ -15,5 +15,34 @@ int main()
         store2, // Store 2's inventory
         store3; // Store 3's inventory
 
-    
+    // Get the beginning inventory for all the stores.
+    cout << "One week ago, 3 new widget stores opened at the same time with the same beginning inventory." << endl;
+    cout << "What was the beginning inventory?" << endl;
+    cin >> begInv;
+
+    // Set each store's inventory.
+    store1 = store2 = store3 = begInv;
+
+    // Get the number of widgets sold at store 1.
+    cout << "How many widgets has store 1 sold?" << endl;
+    cin >> sold;
+    store1 -= sold; // Adjust store 1's inventory.
+
+    // Get the number of widgets sold at store 2.
+    cout << "How many widgets has store 2 sold?" << endl;
+    cin >> sold;
+    store2 -= sold; // Adjust store 2's inventory.
+
+    // Get the number of widgets sold at store 3.
+    cout << "How many widgets has store 3 sold?" << endl;
+    cin >> sold;
+    store3 -= sold; // Adjust store 3's inventory.
+
+    // Display each store's current inventory.
+    cout << "\nThe current inventory of each store: " << endl;
+    cout << "Store 1: " << store1 << endl;
+    cout << "Store 2: " << store2 << endl;
+    cout << "Store 3: " << store3 << endl;
+
+    return 0;
 }
