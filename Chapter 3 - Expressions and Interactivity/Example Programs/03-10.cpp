2fd1548862f99050fd26c7ffff7c0094c1ef8996
Tri <trimpham98@gmail.com>
2022-12-18 13:53:44 -0800

    Add exercise 03-10

---
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-10.cpp
@@ -0,0 +1,18 @@
+// This program uses a type cast expression to print a character
+// from a number.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 65;
+
+    // Display the value of the number variable.
+    cout << number << endl;
+
+    // Display the value of the number converted to the char data type.
+    cout << static_cast<char>(number) << endl;
+
+    return 0;
+}
