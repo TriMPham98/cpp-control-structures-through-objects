9daec647ca9009c9b79a2791ba80679d7e9b8f64
Tri <trimpham98@gmail.com>
2022-12-19 01:00:12 -0800

    Add exercise 03-12

--- Chapter 3 - Expressions and Interactivity/Example Programs/03-12.cpp
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-12.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main()
 {
-    
+    int num1 = 2897, num2 = 5, num3 = 837,
+        num4 = 34, num5 = 7, num6 = 1623,
+        num7 = 390, num8 = 3456, num9 = 12;
+
+    // Display the three rows of numbers
+    cout << num1 << " " << num2 << " " << num3 << endl;
+    cout << num4 << " " << num5 << " " << num6 << endl;
+    cout << num7 << " " << num8 << " " << num9 << endl;
+
+    return 0;
 }
