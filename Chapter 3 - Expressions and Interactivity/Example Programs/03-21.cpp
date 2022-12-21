66d10107d007c4b0f42c7557a41b26b50b6b2623
Tri <trimpham98@gmail.com>
2022-12-21 09:10:54 -0800

    Add exercise 03-21

--- Chapter 3 - Expressions and Interactivity/Example Programs/03-21.cpp
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-21.cpp
@@ -1,2 +1,20 @@
 // This program demonstrates three ways to use cin.get() to pause a program.
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout << "This program has paused. Press Enter to continue." << endl;
+    cin.get(ch);
+    cout << "It has paused a second time. Press Enter again." << endl;
+    ch = cin.get();
+    cout << "It has paused a third time. Press Enter again." << endl;
+    cin.get();
+    cout << "Thank you!" << endl;
+
+    return 0;
+}
