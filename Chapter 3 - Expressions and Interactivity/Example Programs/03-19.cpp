1b2bff95ec99bee430e89af53a339eb3d9734577
Tri <trimpham98@gmail.com>
2022-12-21 08:53:22 -0800

    Add exercise 03-19

--- Chapter 3 - Expressions and Interactivity/Example Programs/03-19.cpp
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-19.cpp
@@ -1,3 +1,24 @@
 // This program demonstrates using the getline function to read
 // character data into a string object.
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    string city;
+
+    cout << "Please enter your name: ";
+    getline(cin, name);
+
+    cout << "Enter the city you live in: ";
+    getline(cin, city);
+
+    cout << "Hello, " << name << endl;
+    cout << "You live in " << city << endl;
+
+    return 0;
+}
