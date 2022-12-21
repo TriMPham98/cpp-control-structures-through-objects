7d77b58e7b0046581a049590e3c9ca6d40f75f90
Tri <trimpham98@gmail.com>
2022-12-21 11:04:15 -0800

    Add exercise 03-23

--- Chapter 3 - Expressions and Interactivity/Example Programs/03-23.cpp
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-23.cpp
@@ -1,2 +1,20 @@
 // This program successfully uses both cin >> and cin.get() for keyboard output.
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+    cin.ignore(); // Skip the newline character
+    cout << "Enter a character: ";
+    ch = cin.get();
+    cout << "Thank you!\n";
+
+    return 0;
+}
