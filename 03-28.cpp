6eacd329ea5615715a4b210e61a4af7943013bc3
Tri <trimpham98@gmail.com>
2022-12-22 09:25:13 -0800

    Add program description

---
+++ 03-28.cpp
@@ -0,0 +1,5 @@
+// This program is used by General Crates, Inc. to calculate
+// the volume, cost, customer charge, and profit of a crate
+// of any size. It calculates this data from user input, which 
+// consists of the dimensions of the crate.
+
