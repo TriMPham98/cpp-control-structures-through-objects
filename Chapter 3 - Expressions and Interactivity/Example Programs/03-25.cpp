b196e9081802f013aecfd8bf2fa9a4d4bc4f02c7
Tri <trimpham98@gmail.com>
2022-12-21 22:18:42 -0800

    Add exercise 03-25

--- Chapter 3 - Expressions and Interactivity/Example Programs/03-25.cpp
+++ Chapter 3 - Expressions and Interactivity/Example Programs/03-25.cpp
@@ -1,2 +1,23 @@
 // This program demonstrates random numbers.
 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    // Get the system time
+    unsigned seed = time(0);
+
+    // Seed the random number generator
+    srand(seed);
+
+    // Display three random numbers
+    cout << rand() << endl;
+    cout << rand() << endl;
+    cout << rand() << endl;
+
+    return 0;
+}
