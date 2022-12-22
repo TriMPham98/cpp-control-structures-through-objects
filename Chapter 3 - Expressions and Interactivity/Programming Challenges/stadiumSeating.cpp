84d7177504bfdadf6aafb2b816e367b5ccc8045e
Tri <trimpham98@gmail.com>
2022-12-22 09:44:35 -0800

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/stadiumSeating.cpp
@@ -0,0 +1,7 @@
+// There are three seating categories at a stadium. For a softball
+// game, Class A seats cost $15, Class B seats cost $12, and Class
+// C seats cost $9. Write a program that asks how many tickets
+// for each class of seats were sold, then displays the amount
+// of income generated from ticket sales. Format your dollar amount
+// in fixed-point notation, with two decimal places of precision,
+// and be sure the decimal point is always displayed.
