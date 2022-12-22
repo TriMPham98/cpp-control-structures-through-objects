cdfb1c3e522fff581b18640e543d9b4803421976
Tri <trimpham98@gmail.com>
2022-12-23 06:50:32 +0800

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/averageRainfall.cpp
@@ -0,0 +1,8 @@
+// Write a program that calculates the average rainfall for three months.
+// The program should ask the user to enter the name of each
+// month, such as June or July, and the amount of rain (in inches)
+// that fell each month. The program should display a message similar
+// to the following: 
+
+// The average rainfall for June, July, and August is 6.72 inches.
+
