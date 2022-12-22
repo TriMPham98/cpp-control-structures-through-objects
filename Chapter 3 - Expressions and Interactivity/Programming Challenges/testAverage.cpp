77dbad25bdf721f501c247d9db837d4cbdf75b42
Tri <trimpham98@gmail.com>
2022-12-23 06:34:00 +0800

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/testAverage.cpp
@@ -0,0 +1,4 @@
+// Write a program that asks for five test scores. 
+// The program should calculate the average test score
+// and display it. The number displayed should be formatted in
+// fixed-point notation, with one decimal point of precision.
