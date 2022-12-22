e2ae2cc3f18cdc7c323a1e13d84be02d416efcc6
Tri <trimpham98@gmail.com>
2022-12-23 06:39:27 +0800

    Add user input and print statements

--- Chapter 3 - Expressions and Interactivity/Programming Challenges/testAverage.cpp
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/testAverage.cpp
@@ -10,5 +10,24 @@ using namespace std;
 
 int main()
 {
-    
+    double test1, test2, test3, test4, test5;
+    double avg;
+
+    cout << "This program calculates the average test score." << endl;
+    cout << "Enter the score for test 1: ";
+    cin >> test1;
+    cout << "Enter the score for test 2: ";
+    cin >> test2;
+    cout << "Enter the score for test 3: ";
+    cin >> test3;
+    cout << "Enter the score for test 4: ";
+    cin >> test4;
+    cout << "Enter the score for test 5: ";
+    cin >> test5;
+
+    avg = (test1 + test2 + test3 + test4 + test5) / 5;
+
+    cout << "The average score for the five test scores is: " << avg << endl;
+
+    return 0;
 }
