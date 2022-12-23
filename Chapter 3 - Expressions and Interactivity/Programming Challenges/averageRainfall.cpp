12633bc92c796c94dae9c4669a79cc717291e0ad
Tri <trimpham98@gmail.com>
2022-12-23 20:14:32 +0700

    Add variable declaration

--- Chapter 3 - Expressions and Interactivity/Programming Challenges/averageRainfall.cpp
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/averageRainfall.cpp
@@ -7,11 +7,17 @@
 // The average rainfall for June, July, and August is 6.72 inches.
 
 #include <iostream>
+#include <string>
 #include <iomanip>
 
 using namespace std;
 
 int main()
 {
+    string month1, month2, month3;
+    double rainMonth1, rainMonth2, rainMonth3;
+    double avg;
+
     cout << "This program calculates the average rainfall for three months." << endl;
+    
 }
