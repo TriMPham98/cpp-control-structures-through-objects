4948b18b97a4cc4d307f2fde3efe5760bfb3f991
Tri <trimpham98@gmail.com>
2022-12-23 20:20:28 +0700

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/maleAndFemalePercentages.cpp
@@ -0,0 +1,11 @@
+// Write a program that asks for the user for the number of 
+// males and the number of females registered in a class.
+// The program should display the percentage of males and 
+// females in the class.
+
+// Hint: Suppose there are 8 males and 12 females in a class.
+// There are 20 students in the class. The percentage of males
+// can be calculated as 8 / 20 = 0.4, or 40 percent. The 
+// percentage of females can be calculated as 12 / 20 = 0.6, 
+// or 60 percent.
+
