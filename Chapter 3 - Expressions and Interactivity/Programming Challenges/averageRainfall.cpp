09c665cf46f86f0e409ec43a73aead4cd7fe4ce6
Tri <trimpham98@gmail.com>
2022-12-23 18:58:33 +0800

    Add boilerplate code

--- Chapter 3 - Expressions and Interactivity/Programming Challenges/averageRainfall.cpp
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/averageRainfall.cpp
@@ -2,7 +2,16 @@
 // The program should ask the user to enter the name of each
 // month, such as June or July, and the amount of rain (in inches)
 // that fell each month. The program should display a message similar
-// to the following: 
+// to the following:
 
 // The average rainfall for June, July, and August is 6.72 inches.
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+}
