ae7c51cb4515777e510a535874c28d7a6b87d5b9
Tri <trimpham98@gmail.com>
2022-12-25 00:04:20 +0700

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/ingredientAdjuster.cpp
@@ -0,0 +1,11 @@
+// A cookie recipe calls for the following ingredients:
+
+// - 1.5 cups of sugar
+// - 1 cup of butter
+// - 2.75 cups of flour
+
+// The recipe produces 48 cookies with this amount of ingredients.
+// Write a program that asks the user how many cookies he or she
+// wants to make, then displays the number of cups of each
+// ingredient needed for the specified number of cookies.
+
