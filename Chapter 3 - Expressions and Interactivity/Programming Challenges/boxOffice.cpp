297efa8e6eac3100ada5008952e82e8378925f4f
Tri <trimpham98@gmail.com>
2022-12-25 14:54:44 +0700

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/boxOffice.cpp
@@ -0,0 +1,15 @@
+// A movie theater only keeps a percentage of the revenue earned
+// from ticket sales. The remainder goes to the movie distributor.
+// Write a program that calculates a theater's gross and net box office 
+// profit for a night. The program should ask for the name of the movie, 
+// and how many adult and child tickets were sold. (The price of
+// an adult ticket is $10.00 and a child's ticket is $6.00.) It 
+// should display a report similar to:
+
+// Movie Name: "Wheels of Fury"
+// Adult Tickets Sold: 382
+// Child Tickets Sold: 127
+// Gross Box Office Profit: $4,582.00
+// Net Box Office Profit: $916.40
+// Amount Paid to Distributor: $3,665.60
+
