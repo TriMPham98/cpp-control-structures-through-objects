6ffe110cf0f844e96b6883cdcbbcad941e3ff41e
Tri <trimpham98@gmail.com>
2022-12-29 10:16:46 +0700

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/howManyWidgets.cpp
@@ -0,0 +1,12 @@
+// The Yukon Widget Company manufactures widgets that weigh 12.5 pounds each.
+// Write a program that calculates how many widgets are stacked on a pallet,
+// based on the total weight of the pallet. The program should ask the user
+// how much the pallet weighs by itself and with the widgets stacked on it.
+// It should then calculate and display the number of widgets stacked on the pallet.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+}
