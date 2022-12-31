ad88f26f61d323f1d4e3ccf711089cc6c6325e9d
Tri <trimpham98@gmail.com>
2022-12-31 11:24:36 +0700

    Add boilerplate code

--- Chapter 3 - Expressions and Interactivity/Programming Challenges/howManyChoices.cpp
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/howManyChoices.cpp
@@ -1,6 +1,14 @@
-// A bag of cookies holds 30 cookies. The calorie information 
-// on the bag claims that there are 10 servings in the bag and 
-// that a serving equals 300 calories. Write a program that asks 
-// the user to input how many cookies he or she actually ate 
+// A bag of cookies holds 30 cookies. The calorie information
+// on the bag claims that there are 10 servings in the bag and
+// that a serving equals 300 calories. Write a program that asks
+// the user to input how many cookies he or she actually ate
 // and then reports how many total calories were consumed.
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
