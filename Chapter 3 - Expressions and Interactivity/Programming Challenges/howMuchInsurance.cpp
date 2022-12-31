837e43def2ae615fe79ed5c3f4ac0c2247c80d89
Tri <trimpham98@gmail.com>
2022-12-31 23:14:24 +0800

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/howMuchInsurance.cpp
@@ -0,0 +1,6 @@
+// Many financial  experts advise that property owners should
+// insure their homes or buildings for at least 80 percent
+// of the amount it would cost to replace the structure. Write
+// a program that asks for the user to enter the replacement cost
+// of a building, then displays the minimum amount of insurance
+// he or she should buy for the property.
