226bac75853dd8b962873b924162f3e21ad61f0d
Tri <trimpham98@gmail.com>
2022-12-31 11:22:37 +0700

    Add program description

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/howManyChoices.cpp
@@ -0,0 +1,6 @@
+// A bag of cookies holds 30 cookies. The calorie information 
+// on the bag claims that there are 10 servings in the bag and 
+// that a serving equals 300 calories. Write a program that asks 
+// the user to input how many cookies he or she actually ate 
+// and then reports how many total calories were consumed.
+
