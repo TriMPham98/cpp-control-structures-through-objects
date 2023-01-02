18be43c3e0f68633ac9f3a5ae03e3a9ba358efe5
Tri <trimpham98@gmail.com>
2023-01-02 01:12:45 -0800

    Add variable declarations, user input, and print statements

--- Chapter 3 - Expressions and Interactivity/Programming Challenges/automobileCosts.cpp
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/automobileCosts.cpp
@@ -11,5 +11,24 @@ using namespace std;
 
 int main()
 {
-    
+    double loan, insurance, gas, oil, tires, maintainence, total;
+
+    cout << "This program calculates the monthly operating costs for your car." << endl;
+    cout << "What's the cost of the loan?" << endl;
+    cin >> loan;
+    cout << "What's the cost of the insurance?" << endl;
+    cin >> insurance;
+    cout << "What's the cost of the gas?" << endl;
+    cin >> gas;
+    cout << "What's the cost of the oil?" << endl;
+    cin >> oil;
+    cout << "What's the cost of the tires?" << endl;
+    cin >> tires;
+    cout << "What's the cost of the maintainence?" << endl;
+    cin >> maintainence;
+
+    total = loan + insurance + gas + oil + tires + maintainence;
+
+    cout << fixed << setprecision(2);
+    cout << "The monthly total for operating your car is $" << total << endl;
 }
