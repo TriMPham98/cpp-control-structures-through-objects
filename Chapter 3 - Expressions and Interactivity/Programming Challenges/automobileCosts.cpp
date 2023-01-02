f94e78eb1f2844204506dca2cb5be2f870471d7c
Tri <trimpham98@gmail.com>
2023-01-01 23:23:15 -0800

    Add boilerplate code

---
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/automobileCosts.cpp
@@ -0,0 +1,15 @@
+// Write a program that asks the user to enter the monthly costs for the
+// following expenses incurred from operating his or her automobile:
+// loan payment, insurance, gas, oil, tires, and maintainence. The program
+// should then display the total monthly cost of these expenses, and the
+// total annual cost of these expenses.
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    
+}
