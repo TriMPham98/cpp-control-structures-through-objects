1c662568155b89e03d01ecd9117ff128a3b64918
Tri <trimpham98@gmail.com>
2023-01-02 17:51:55 -0800

    Add program description

---
+++ currency.cpp
@@ -0,0 +1,7 @@
+// Write a program that will convert U.S. dollar amounts to Japanese yen and to
+// euros, storing the conversion factors in the constants YEN_PER_DOLLAR and
+// EUROS_PER_DOLLAR. To get the most up-to-date exchange rates, search the Internet'
+// using the term "currency exchange rate". If you cannot find the most recent exchange
+// rates, use the following:
+// 1 Dollar = 98.93 Yen
+// 1 Dollar = 0.74 Euros
