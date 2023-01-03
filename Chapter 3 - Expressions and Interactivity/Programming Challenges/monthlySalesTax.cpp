37bd026de61de6a4f49b4407ff666f3604849f58
Tri <trimpham98@gmail.com>
2023-01-02 18:15:24 -0800

    Add boilerplate code

--- Chapter 3 - Expressions and Interactivity/Programming Challenges/monthlySalesTax.cpp
+++ Chapter 3 - Expressions and Interactivity/Programming Challenges/monthlySalesTax.cpp
@@ -2,12 +2,12 @@
 // for the month and the amount of sales tax collected. Write a program that
 // asks for the month, the year, and the total amount collected at the cash
 // register (i.e. sales plus sales tax). Assume the state tax is 4 percent,
-// and the county sales tax is 2 percent. If the total amount collected is known 
-// and the total sales tax is 6 percent, the amount of product sales may be 
+// and the county sales tax is 2 percent. If the total amount collected is known
+// and the total sales tax is 6 percent, the amount of product sales may be
 // calculated as S = (T / 1.06)
 // S is the product sales and T is the total income (product sales plus sales
 // tax). The program should display a report similar to:
-// 
+//
 // Month: October
 // -------------------------
 // Total Collected:     $ 26572.89
@@ -15,3 +15,12 @@
 // County Sales Tax:    $ 501.38
 // State Sales Tax:     $ 1002.75
 // Total Sales Tax:     $ 1504.13
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+}
