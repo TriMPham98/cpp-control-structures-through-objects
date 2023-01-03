3a6dc9990ac91bfd44aa094a69bd49c0f85a825d
Tri <trimpham98@gmail.com>
2023-01-02 17:44:38 -0800

    Add program description

---
+++ celsiusToFahrenheit.cpp
@@ -0,0 +1,4 @@
+// Write a program that converts Celsius temperatures to Fahrenheit temperatures.
+// The formula is F = [(9/5) * C] + 32
+// F is the Fahrenheit temperature, and C is the Celsius temperature.
+
