51eff015171724615ddc47f853f19f1ec620160b
Tri <trimpham98@gmail.com>
2023-01-04 04:05:48 -0800

    Add program description

---
+++ chapter3_expressionsAndInteractivity/programmingChallenges/seniorCitizenPropertyTax.cpp
@@ -0,0 +1,12 @@
+// Madison County provides a $5,000 homestead exemption for senior citizens.
+// For example, if a senior's house is value at $158,000, the assessment value
+// is $94,800, as explained above. However, he would only pay tax on $89,800.
+// At last year's tax rate of $2.64 per $100 of assessment value, the tax
+// would be $2,370.72. In addition to the tax break, senior citizens are allowed
+// to pay their property tax in four equal payments. The quarterly payment due
+// on this property would be $592.68. Write a program that asks for the actual
+// value of a piece of property and the current tax rate for each $100 of
+// assessed value. The program should then calculate and report how much annual
+// property tax a senior homeowner will be charged for this property, and what
+// the quarterly tax bill will be.
+
