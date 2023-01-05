87802d8f8927f51d3cd01ec142e7ffda6b88cbd2
Tri <trimpham98@gmail.com>
2023-01-04 23:58:58 -0800

    Add boilerplate code

---
+++ chapter3_expressionsAndInteractivity/programmingChallenges/interestEarned.cpp
@@ -0,0 +1,25 @@
+// Assuming there are no deposits other than the original investment,
+// the balance in a savings account after one year may be calculated as
+// Amount = Principal * (1 + Rate/T) ^ (T * Years)
+// Principal is the balance in the savings account, Rate is the annual
+// interest rate, and T is the number of times the interest is compounded
+// during a year. T is 4 if the interest is compounded quarterly.
+
+// Write a program that asks for the principal, the rate, and the number
+// of times the interest is compounded. The program should display a report
+// similar to:
+
+// Interest Rate: 4.25%
+// Times Compounded: 12
+// Principal: $ 1000.00
+// Interest: $ 43.34
+// Amount in Savings: $ 1043.34
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+}
