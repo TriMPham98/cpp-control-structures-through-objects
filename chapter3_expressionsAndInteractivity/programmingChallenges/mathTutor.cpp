ba4a5b7300993e025325cfded5aa3a59c02f6cff
Tri <trimpham98@gmail.com>
2023-01-04 23:33:33 -0800

    Add program description

---
+++ chapter3_expressionsAndInteractivity/programmingChallenges/mathTutor.cpp
@@ -0,0 +1,13 @@
+// Write a program that can be used as a math tutor for a young student. 
+// The program should display two random numbers to be added, such as:
+// 247
+// 129
+// ----
+// The program should then pause while the student works on the problem.
+// When the student is ready to check the answer, he or she can press a key
+// and the program will display the correct solution:
+// 247
+// 129
+// ----
+// 376
+
