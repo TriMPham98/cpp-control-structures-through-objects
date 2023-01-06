a82a5e596250164fef0ada5987994dae01c45d52
Tri <trimpham98@gmail.com>
2023-01-05 21:43:13 -0800

    Add program description

--- chapter3_expressionsAndInteractivity/programmingChallenges/pizzaPi.cpp
+++ chapter3_expressionsAndInteractivity/programmingChallenges/pizzaPi.cpp
@@ -3,3 +3,11 @@
 // A) Ask the user for the diameter of the pizza in inches.
 // B) Calculate the number of slices that may be taken from a pizza of that size.
 // C) Display a message telling the number of slices.
+// To calculate the number of slices that may be taken from the pizza, you must know
+// the following facts:
+// - Each slice should have an area of 14.125 inches.
+// - To calculate the number of slices, simply divide the area of the pizza by 14.125.
+// - The area of the pizza is calculated with this formula:
+// Area = PI * r ^ 2
+// Note: PI is the Greek letter pi. 3.14159 can be used as its value. The variable r
+// is the radius of the pizza. Divide the diameter by 2 to get the radius.
