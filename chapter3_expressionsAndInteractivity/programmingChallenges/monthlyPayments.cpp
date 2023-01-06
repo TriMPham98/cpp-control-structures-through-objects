379b2c0a0364ccd964ac791453198cd2d6e72b32
Tri <trimpham98@gmail.com>
2023-01-05 20:03:32 -0800

    Add variable declarations, user input, and print statements

--- chapter3_expressionsAndInteractivity/programmingChallenges/monthlyPayments.cpp
+++ chapter3_expressionsAndInteractivity/programmingChallenges/monthlyPayments.cpp
@@ -13,9 +13,37 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+
+    double Payment, Rate, N, L, AmountPaidBack, InterestPaid;
+
+    cout << "Enter the loan amount: ";
+    cin >> L;
+
+    cout << "Enter the monthly interest rate (in percent): ";
+    cin >> Rate;
+    Rate /= 100; // convert percentage to decimal
+
+    cout << "Enter the number of payments: ";
+    cin >> N;
+
+    Payment = (Rate * pow((1 + Rate), N)) / (pow((1 + Rate), N) - 1) * L;
+
+    AmountPaidBack = Payment * N;
+    InterestPaid = AmountPaidBack - L;
+
+    cout << fixed << setprecision(2);
+    cout << "Loan Amount: $" << setw(10) << L << endl;
+    cout << "Monthly Interest Rate: " << setw(10) << Rate << "%" << endl;
+    cout << "Number of Payments: " << setw(10) << N << endl;
+    cout << "Monthly Payment: $" << setw(10) << Payment << endl;
+    cout << "Amount Paid Back: $" << setw(10) << AmountPaidBack << endl;
+    cout << "Interest Paid: $" << setw(10) << InterestPaid << endl;
+
+    return 0;
 }
