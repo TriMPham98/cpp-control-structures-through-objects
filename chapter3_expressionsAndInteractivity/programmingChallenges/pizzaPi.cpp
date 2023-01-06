b8c9c5082c656bfb28466bdbb2106f34a97bc465
Tri <trimpham98@gmail.com>
2023-01-05 21:41:16 -0800

    Add program description

--- chapter3_expressionsAndInteractivity/programmingChallenges/pizzaPi.cpp
+++ chapter3_expressionsAndInteractivity/programmingChallenges/pizzaPi.cpp
@@ -1,2 +1,5 @@
 // Joe's Pizza Palace needs a program to calculate the number of slices a pizza of any
 // size can be divided into. The program should perform the following steps:
+// A) Ask the user for the diameter of the pizza in inches.
+// B) Calculate the number of slices that may be taken from a pizza of that size.
+// C) Display a message telling the number of slices.
