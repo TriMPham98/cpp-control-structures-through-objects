d7531938af3b1bd3dc28cd131d17b4e3931f39f5
Tri <trimpham98@gmail.com>
2023-01-07 21:58:36 -0800

    Add program description

---
+++ chapter3_expressionsAndInteractivity/programmingChallenges/howManyPizzas.cpp
@@ -0,0 +1,7 @@
+// Modify the program you wrote in Programming Challenge 20 (Pizza Pi)
+// so it reports the number of pizzas you need to buy for a party if 
+// each person attending is expected to eat an average of four slices.
+// The program should ask the user for the number of people who will be
+// at the party, and for the diameter of the pizzas to be ordered. It 
+// should then calculate and display the number of pizzas to purchase.
+
