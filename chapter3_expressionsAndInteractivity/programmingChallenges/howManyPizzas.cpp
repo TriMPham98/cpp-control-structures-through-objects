17fef6a95c0fa5357603f8d6c603ae8daa31d92c
Tri <trimpham98@gmail.com>
2023-01-07 21:59:11 -0800

    Add boilerplate code

--- chapter3_expressionsAndInteractivity/programmingChallenges/howManyPizzas.cpp
+++ chapter3_expressionsAndInteractivity/programmingChallenges/howManyPizzas.cpp
@@ -1,7 +1,15 @@
 // Modify the program you wrote in Programming Challenge 20 (Pizza Pi)
-// so it reports the number of pizzas you need to buy for a party if 
+// so it reports the number of pizzas you need to buy for a party if
 // each person attending is expected to eat an average of four slices.
 // The program should ask the user for the number of people who will be
-// at the party, and for the diameter of the pizzas to be ordered. It 
+// at the party, and for the diameter of the pizzas to be ordered. It
 // should then calculate and display the number of pizzas to purchase.
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+}
