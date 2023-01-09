bf16c51b6e1add01678214f29650e1dd452368c7
Tri <trimpham98@gmail.com>
2023-01-09 01:18:35 -0800

    Add program description

---
+++ chapter3_expressionsAndInteractivity/programmingChallenges/angleCalculator.cpp
@@ -0,0 +1,5 @@
+// Write a program that asks the user for an angle, entered in radians. The program
+// should then display the sine, cosine, and tangent of the angle. (Use the sin,
+// cos, and tan library functions to determine these values.) The output should
+// be displayed in fixed-point notation, rounded to four decimal places of 
+// precision.
