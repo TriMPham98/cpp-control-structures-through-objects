974b3e34591f0c2b7dfe3789e54e6dfed871e1c6
Tri <trimpham98@gmail.com>
2023-01-11 00:20:28 -0800

    Add program description

--- chapter3_expressionsAndInteractivity/programmingChallenges/stockTransactionProgram.cpp
+++ chapter3_expressionsAndInteractivity/programmingChallenges/stockTransactionProgram.cpp
@@ -4,3 +4,9 @@
 // - When Joe purchased the stock, he paid $45.50 per share.
 // - Joe paid his stockbroker a commission that amounted to 2 percent of the 
 //   amount he paid for the stock.
+// Two weeks later, Joe sold the stock. Here are the details of the sale:
+// - The number of shares that Joe sold was 1,000.
+// - He sold the stock for $56.90 per share.
+// - He paid the stockbroker another commission that amounted to 2 percent of 
+//   the amount he received for the stock.
+ 
