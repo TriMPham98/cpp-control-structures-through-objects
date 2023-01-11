dd878cf75079b9a26be40349b07f46ed2567742a
Tri <trimpham98@gmail.com>
2023-01-11 00:19:02 -0800

    Add program description

---
+++ chapter3_expressionsAndInteractivity/programmingChallenges/stockTransactionProgram.cpp
@@ -0,0 +1,6 @@
+// Last month, Joe purchased some stock in Acme Software, Inc. Here are the 
+// details of the purchase:
+// - The number of shares that Joe purchased was 1,000.
+// - When Joe purchased the stock, he paid $45.50 per share.
+// - Joe paid his stockbroker a commission that amounted to 2 percent of the 
+//   amount he paid for the stock.
