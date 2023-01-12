8858fce863dbac8f8c89ea023b209f00360eec57
Tri <trimpham98@gmail.com>
2023-01-11 17:05:01 -0800

    Add program description

---
+++ chapter3_expressionsAndInteractivity/programmingChallenges/wordGame.cpp
@@ -0,0 +1,13 @@
+// Write a program that plays a word game with the user. The program should ask the 
+// user to enter the following:
+// - His or her name
+// - His or her age
+// - The name of a city
+// - The name of a college
+// - A profession
+// - A type of animal
+// - A pet's name
+
+// After the user has entered these items, the program should display the following story,
+// inserting the user's input into the appropriate locations.
+
