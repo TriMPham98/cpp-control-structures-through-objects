89178fc63764eebd917903bcd0e95ba8ab1b06a0
Tri <trimpham98@gmail.com>
2023-01-11 17:06:41 -0800

    Add program description

--- chapter3_expressionsAndInteractivity/programmingChallenges/wordGame.cpp
+++ chapter3_expressionsAndInteractivity/programmingChallenges/wordGame.cpp
@@ -11,3 +11,6 @@
 // After the user has entered these items, the program should display the following story,
 // inserting the user's input into the appropriate locations.
 
+// There once was a person named [NAME] who lived in a [CITY]. At the age of [AGE],
+// [NAME] went to college at [COLLEGE]. [NAME] graduated and went to work as a [PROFESSION].
+// Then, [NAME] adopted a(n) [ANIMAL] named [PETNAME]. They both lived happily ever after!
