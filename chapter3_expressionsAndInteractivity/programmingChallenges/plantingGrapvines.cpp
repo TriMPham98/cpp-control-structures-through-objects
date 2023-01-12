249662b77028004823990268d8fd1a979161e0d5
Tri <trimpham98@gmail.com>
2023-01-11 16:48:15 -0800

    Add program description

---
+++ chapter3_expressionsAndInteractivity/programmingChallenges/plantingGrapvines.cpp
@@ -0,0 +1,5 @@
+// A vineyard owner is planting several new rows of grapevines, and needs to 
+// know how many grapevines to plant in each row. She has determined that 
+// after measuring the length of a future row, she can use the following formula
+// to calculate the number of vines that will fit in the row, along with the trellis
+// end-post assemblies that will need to be constructed at each end of the row:
