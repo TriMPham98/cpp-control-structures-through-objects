bd22ce55d3d14ddafab43448c02af85e6b39b515
Tri <trimpham98@gmail.com>
2023-01-11 16:51:33 -0800

    Add boilerplate code

--- chapter3_expressionsAndInteractivity/programmingChallenges/plantingGrapvines.cpp
+++ chapter3_expressionsAndInteractivity/programmingChallenges/plantingGrapvines.cpp
@@ -1,5 +1,5 @@
-// A vineyard owner is planting several new rows of grapevines, and needs to 
-// know how many grapevines to plant in each row. She has determined that 
+// A vineyard owner is planting several new rows of grapevines, and needs to
+// know how many grapevines to plant in each row. She has determined that
 // after measuring the length of a future row, she can use the following formula
 // to calculate the number of vines that will fit in the row, along with the trellis
 // end-post assemblies that will need to be constructed at each end of the row:
@@ -8,7 +8,7 @@
 
 // The terms in the formula are:
 // V is the number of grapevines that will fit in the row.
-// R is the length of the row, in feet. 
+// R is the length of the row, in feet.
 // E is the amount of space, in feet, used by an end-post assembly.
 // S is the space between vines, in feet.
 
@@ -20,3 +20,12 @@
 
 // Once the input data has been entered, the program should calculate and display
 // the number of grapevines that will fit in the row.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
