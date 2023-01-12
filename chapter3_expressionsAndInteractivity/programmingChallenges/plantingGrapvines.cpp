349e4d1d25c9781fc6e7d43bbdeea57842ec9b2f
Tri <trimpham98@gmail.com>
2023-01-11 16:49:19 -0800

    Add program description

--- chapter3_expressionsAndInteractivity/programmingChallenges/plantingGrapvines.cpp
+++ chapter3_expressionsAndInteractivity/programmingChallenges/plantingGrapvines.cpp
@@ -3,3 +3,11 @@
 // after measuring the length of a future row, she can use the following formula
 // to calculate the number of vines that will fit in the row, along with the trellis
 // end-post assemblies that will need to be constructed at each end of the row:
+
+// V = (R - 2E) / S
+
+// The terms in the formula are:
+// V is the number of grapevines that will fit in the row.
+// R is the length of the row, in feet. 
+// E is the amount of space, in feet, used by an end-post assembly.
+// S is the space between vines, in feet.
