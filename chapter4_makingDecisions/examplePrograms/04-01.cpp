cae453ea37b443a7c84c5c742fd049e8663c9ccd
Tri <trimpham98@gmail.com>
2023-01-11 21:08:10 -0800

    Declare variables

--- chapter4_makingDecisions/examplePrograms/04-01.cpp
+++ chapter4_makingDecisions/examplePrograms/04-01.cpp
@@ -8,4 +8,10 @@ int main()
 {
     bool trueValue, falseValue;
     int x = 5, y = 10;
+
+    trueValue = x < y;
+    falseValue = y == x;
+
+    cout << "True is " << trueValue << endl;
+    cout << "False is " << falseValue << endl;
 }
