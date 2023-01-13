05ccbcf16e28656b661500d6ea57252deff8dbdb
Tri <trimpham98@gmail.com>
2023-01-13 07:55:34 -0800

    Add conditional to check if a == b

--- chapter4_makingDecisions/examplePrograms/04-04.cpp
+++ chapter4_makingDecisions/examplePrograms/04-04.cpp
@@ -9,4 +9,16 @@ int main()
 {
     double a = 1.5; // a is 1.5.
     double b = 1.5; // b is 1.5.
+
+    a += 0.00000000000000001; // Add a little to a.
+    if (a == b)
+    {
+        cout << "Both a and b are the same." << endl;
+    }
+    else
+    {
+        cout << "a and b are not the same." << endl;
+    }
+
+    return 0;
 }
