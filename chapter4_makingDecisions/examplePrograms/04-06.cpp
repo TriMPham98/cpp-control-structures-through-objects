6b3bd7958ee86c265c4f25e903abc55c9faf88bc
Tri <trimpham98@gmail.com>
2023-01-13 13:41:47 -0800

    Add boilerplate code

--- chapter4_makingDecisions/examplePrograms/04-06.cpp
+++ chapter4_makingDecisions/examplePrograms/04-06.cpp
@@ -1,2 +1,11 @@
 // This program averages 3 test scores. It determines an if statement executing
 // a block of statements
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+}
