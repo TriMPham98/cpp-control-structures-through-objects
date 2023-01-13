93076e520e09886a2dbbde38842b9a758de5223c
Tri <trimpham98@gmail.com>
2023-01-13 07:53:34 -0800

    Add boilerplate code

--- chapter4_makingDecisions/examplePrograms/04-04.cpp
+++ chapter4_makingDecisions/examplePrograms/04-04.cpp
@@ -1,2 +1,10 @@
-// This program demonstrates how floating-point round-off errors can make 
+// This program demonstrates how floating-point round-off errors can make
 // equality operations unreliable.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+}
