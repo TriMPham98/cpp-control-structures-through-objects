1599111a76d94d91a1a687f9a174e586db930880
Tri <trimpham98@gmail.com>
2023-01-13 13:43:27 -0800

    Add variable declarations

--- chapter4_makingDecisions/examplePrograms/04-06.cpp
+++ chapter4_makingDecisions/examplePrograms/04-06.cpp
@@ -8,4 +8,7 @@ using namespace std;
 
 int main()
 {
+    const int HIGH_SCORE = 95;  // A high score is 95 or greater
+    int score1, score2, score3; // To hold three test scores
+    double average;             // To hold the average score
 }
