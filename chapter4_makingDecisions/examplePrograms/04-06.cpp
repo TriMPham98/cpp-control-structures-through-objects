e71814588dcfa911d9a35ff60a9ffd18362bae54
Tri <trimpham98@gmail.com>
2023-01-13 13:53:54 -0800

    Add user input

--- chapter4_makingDecisions/examplePrograms/04-06.cpp
+++ chapter4_makingDecisions/examplePrograms/04-06.cpp
@@ -11,4 +11,10 @@ int main()
     const int HIGH_SCORE = 95;  // A high score is 95 or greater
     int score1, score2, score3; // To hold three test scores
     double average;             // To hold the average score
+
+    // Get the three test scores.
+    cout << "Enter 3 test scores and I will average them: ";
+    cin >> score1 >> score2 >> score3;
+
+    
 }
