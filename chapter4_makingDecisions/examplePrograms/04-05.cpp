7b9c18d62c10e0029cf87f1717f5828ef56180b7
Tri <trimpham98@gmail.com>
2023-01-13 13:35:33 -0800

    Add boilerplate code

--- chapter4_makingDecisions/examplePrograms/04-05.cpp
+++ chapter4_makingDecisions/examplePrograms/04-05.cpp
@@ -1,3 +1,11 @@
 // This program averages 3 test scores. The if statement uses the = operator,
-// but the == operator was intended. 
+// but the == operator was intended.
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+}
