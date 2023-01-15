48fb517e7906996c4448d7dd73fe4ac4f327593b
Tri <trimpham98@gmail.com>
2023-01-15 08:26:13 -0800

    Add program description

---
+++ chapter4_makingDecisions/examplePrograms/04-08.cpp
@@ -0,0 +1,3 @@
+// This program uses the modulus operator to determine if a number is odd or even.
+// If the number is evenly divisible by 2, it is an even number. A remainder 
+// indicates it is odd. 
