a0ae5b56078b08d1a746bc4b7ace6a7929e2088f
Tri <trimpham98@gmail.com>
2023-01-15 08:26:44 -0800

    Add boilerplate code

--- chapter4_makingDecisions/examplePrograms/04-08.cpp
+++ chapter4_makingDecisions/examplePrograms/04-08.cpp
@@ -1,3 +1,11 @@
 // This program uses the modulus operator to determine if a number is odd or even.
-// If the number is evenly divisible by 2, it is an even number. A remainder 
-// indicates it is odd. 
+// If the number is evenly divisible by 2, it is an even number. A remainder
+// indicates it is odd.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+}
