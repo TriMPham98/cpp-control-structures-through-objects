bb26d775cde9519e2a828632fc16aa34f2f0f33d
Tri <trimpham98@gmail.com>
2023-01-15 08:28:39 -0800

    Add even or odd conditional and print statements

--- chapter4_makingDecisions/examplePrograms/04-08.cpp
+++ chapter4_makingDecisions/examplePrograms/04-08.cpp
@@ -13,5 +13,14 @@ int main()
     cout << "Enter an integer and I will tell you if it is odd or even." << endl;
     cin >> number;
 
-    
+    if (number % 2 == 0)
+    {
+        cout << number << " is even." << endl;
+    }
+    else
+    {
+        cout << number << " is odd." << endl;
+    }
+
+    return 0;
 }
