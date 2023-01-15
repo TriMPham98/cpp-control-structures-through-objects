8e98625255990707bc3e8201c04982890e25e8fc
Tri <trimpham98@gmail.com>
2023-01-15 08:32:09 -0800

    Add boilerplate code

--- 04-09.cpp
+++ 04-09.cpp
@@ -2,3 +2,10 @@
 // num2 and the result is displayed. Before the division operation, however, num2
 // is tested for the value 0. If it contains 0, the division does not take place.
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+}
