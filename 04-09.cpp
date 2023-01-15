9aedda8b47e9be956f3a2d08cfc1f5b695a5b4e4
Tri <trimpham98@gmail.com>
2023-01-15 08:31:39 -0800

    Add program description

---
+++ 04-09.cpp
@@ -0,0 +1,4 @@
+// This program asks the user for two numbers, num1 and num2. num1 is divided by
+// num2 and the result is displayed. Before the division operation, however, num2
+// is tested for the value 0. If it contains 0, the division does not take place.
+
