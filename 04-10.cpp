0d38e86fa1111f67dca19e78a03ab37c8db8b015
Tri <trimpham98@gmail.com>
2023-01-16 09:22:10 -0800

    Add user input

--- 04-10.cpp
+++ 04-10.cpp
@@ -6,8 +6,13 @@ using namespace std;
 
 int main()
 {
-    char employed, // Currently employed, Y or N
-    recentGrad; // Recent graduate, Y or N
+    char employed,  // Currently employed, Y or N
+        recentGrad; // Recent graduate, Y or N
 
-    
+    // Is the user employed and a recent graduate?
+    cout << "Answer the following questions with either Y for Yes or N for No." << endl;
+    cout << "Are you employed? ";
+    cin >> employed;
+    cout << "Have you graduated from college in the past two years? ";
+    cin >> recentGrad;
 }
