c77a827c00a008f9d7cd239987d630eaf8f12d1a
Tri <trimpham98@gmail.com>
2023-01-16 09:50:50 -0800

    Add boilerplate code

--- chapter4_makingDecisions/examplePrograms/04-12.cpp
+++ chapter4_makingDecisions/examplePrograms/04-12.cpp
@@ -1,2 +1,10 @@
-// This program uses nested if/else statements to assign a letter grade 
+// This program uses nested if/else statements to assign a letter grade
 // (A, B, C, D, or F) to start a numeric test score.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+}
