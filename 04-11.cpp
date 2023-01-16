397d97696705805c86178bb4f9c174a044912045
Tri <trimpham98@gmail.com>
2023-01-16 09:31:12 -0800

    Add nested if statement

--- 04-11.cpp
+++ 04-11.cpp
@@ -15,4 +15,23 @@ int main()
     cin >> employed;
     cout << "Have you graduated from college in the past two years? ";
     cin >> recentGrad;
+
+    // Determine the user's loan qualifications.
+    if (employed == 'Y')
+    {
+        if (recentGrad == 'Y')
+        {
+            cout << "You qualify for the special interest rate." << endl;
+        }
+        else // Not a recent grad, but employed
+        {
+            cout << "You must have graduated from college in the past two years to qualify." << endl;
+        }
+    }
+    else
+    {
+        cout << "You must be employed to qualify." << endl;
+    }
+
+    return 0;
 }
