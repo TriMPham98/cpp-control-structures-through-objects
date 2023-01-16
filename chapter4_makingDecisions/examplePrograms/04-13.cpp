240050400bccb4d3e1286f7bd6e8f69c5c7ede9f
Tri <trimpham98@gmail.com>
2023-01-16 09:58:37 -0800

    Add boilerplate code

--- chapter4_makingDecisions/examplePrograms/04-13.cpp
+++ chapter4_makingDecisions/examplePrograms/04-13.cpp
@@ -1,3 +1,10 @@
 // This program uses an if/else if statement to assign a letter grade (A, B, C, D,
 // or F) to a numeric test score.
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+}
