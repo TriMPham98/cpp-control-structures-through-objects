47a2de7765da2d9c9b0bff911fd2bcd785a28e1f
Tri <trimpham98@gmail.com>
2023-01-26 17:25:10 -0800

    Add user input

--- chapter4_makingDecisions/examplePrograms/04-15.cpp
+++ chapter4_makingDecisions/examplePrograms/04-15.cpp
@@ -8,5 +8,9 @@ int main()
 {
     char employed,  // Currently employed, Y or N
         recentGrad; // Recent graduate, Y or N
-        
+
+    // Is the user employed and a recent graduate?
+    cout << "Answer the following questions with either Y for Yes or N for No." << endl;
+    cout << "Are you employed? ";
+    cin >> employed;
 }
